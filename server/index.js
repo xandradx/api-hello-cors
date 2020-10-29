9ad30f2e191182ad1bf8d4c61405c74618f73c54
Jorge Andrade <jandrade@i-t-m.com>
2020-10-29 01:08:38 -0600

    Revert "Remove CORS"
    This reverts commit 7f48e369741eb59082bfc23b19b1d3a551d60f80.

--- server/index.js
+++ server/index.js
@@ -7,12 +7,18 @@ const port = process.env.SERVER_PORT || 8080;
 
 const morgan = require('morgan');
 
+/*
+app.use(function(req,res,next){
+	res.header("Access-Control-Allow-Origin","*")
+	res.header("Access-Control-Allow-Headers","Origin,X-Requested-With,Content-type,Accept");next();
+});
+*/
 console.log(`Running on port ${port}`);
 app.use(morgan('combined'));
-app.use(cors({
-  origin: "restricted.hostname"
+/*app.use(cors({
+  origin: "*"
 }));
-
+*/
 app.get("/api/ping", (req, res) => {
   res.send({
     msg: "Hello, World"
