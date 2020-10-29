ced39c0db2dd29d0f575e72e005ba535f7b712b1
Jorge Andrade <jandrade@i-t-m.com>
2020-10-28 23:40:53 -0600

    CORS enabled for *

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const debug = require("debug")("server");
+const cors = require("cors");
+
+const app = express();
+const port = process.env.SERVER_PORT || 3001;
+
+/*
+app.use(function(req,res,next){
+	res.header("Access-Control-Allow-Origin","*")
+	res.header("Access-Control-Allow-Headers","Origin,X-Requested-With,Content-type,Accept");next();
+});
+*/
+console.log(`Running on port ${port}`);
+app.use(cors({
+  origin: "*"
+}));
+app.get("/api/ping", (req, res) => {
+  res.send({
+    msg: "Hello, World"
+  });
+});
+
+app.listen(port, () => debug(`Listening on port ${port}`));
